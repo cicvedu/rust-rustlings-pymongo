943db8981fcd83e2f2ec28ff44cd95854fe13daf
wuaoxiang <os.popen@gmail.com>
2023-08-07 10:28:24 +0800

    solve drive5

--- exercises/drive/drive5.rs
+++ exercises/drive/drive5.rs
@@ -5,21 +5,26 @@
 // You should not modify any existing code. All you need to do is add two line of attributes.
 
 
-// I AM NOT DONE
-pub use Foo::my_demo_function;
+
+
 
 extern  {
-    #[link(name = "my_demo_function")]
+    // #[link_name = "my_demo_function"]
     #[no_mangle]
     fn my_demo_function(a:u32) -> u32;
-    #[link(name = "my_demo_function")]
+    // #[link(name = "my_demo_function")]
     #[no_mangle]
+    // link(name) means link of dylib
+    // link_name  means link of function name
+    // #[link(name = "my_demo_function")]
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a:u32) -> u32;
 }
 
 
 mod Foo{
-    pub fn my_demo_function(a:u32) -> u32 {a}
+    #[no_mangle]
+    fn my_demo_function(a:u32) -> u32 {a}
 }
 
 #[cfg(test)]
